98e750d6f5ca8b5d9df68fa5ddf5bb6f684dfe1b
Fabio Caldarini <fabio.caldarini99@gmail.com>
2023-10-12 17:02:08 +0200

    s3-g4-esercizio

---
+++ m1/s3/g4/js/d9.js
@@ -0,0 +1,16 @@
+
+
+    function tableCreate() {
+    const body = document.querySelector('body');
+    tabella = document.createElement('table'); 
+
+        for (let i = 1; i < 76; index++) {
+            const tr = tabella.insertRow();
+            for (let index = 0; index < array.length; index++) {
+                const element = array[index];
+                
+            }
+            
+        }
+
+    }
